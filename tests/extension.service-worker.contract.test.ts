921134b7c1782b14344b05e39e0d391ee76e7a5b
mcontheway <mc0919@gmail.com>
2026-04-07 16:18:07 +0800

    test(contract): 恢复 service-worker 默认聚合入口

--- tests/extension.service-worker.contract.test.ts
+++ tests/extension.service-worker.contract.test.ts
@@ -1,1 +1,4 @@
 import "./extension.service-worker.bootstrap-trust.suite.js";
+import "./extension.service-worker.gate-approval.suite.js";
+import "./extension.service-worker.recovery-relay.suite.js";
+import "./extension.service-worker.signature-editor.suite.js";
